a9254fe2d1896d70a3a3bbd7544b285a2335fd9d
Grzegorz Paprzycki <grzegorz.paprzycki@capgemini.com>
2024-02-29 16:40:08 +0100

    adapt styles and add date picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import "./App.css";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import HorizontalStepper from "./components/HorizontalStepper";
+import { Container } from "@mui/material";
 
 function App() {
-  return <HorizontalStepper />;
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Container maxWidth="sm">
+        <HorizontalStepper />
+      </Container>
+    </LocalizationProvider>
+  );
 }
 
 export default App;
--- src/components/Step0/index.tsx
+++ src/components/Step0/index.tsx
@@ -1,3 +1,10 @@
+import { Stack } from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers";
+
 export default function Step0() {
-  return <div>This is Step 0</div>;
+  return (
+    <Stack spacing={4} mt={4}>
+      <DatePicker format="DD/MM/YYYY" label="Data urodzenia" />
+    </Stack>
+  );
 }
