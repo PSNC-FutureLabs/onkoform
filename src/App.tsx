27e98fa32e10185e8a518c091154c974dd82e1d4
Krzysztof Wlodarski <krzysztof.wlodarski@capgemini.com>
2024-05-16 12:19:06 +0200

    padding added to Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import HorizontalStepper from "./components/HorizontalStepper";
@@ -8,27 +7,28 @@ import { DevTool } from "@hookform/devtools";
 import { FormFields, schema } from "./business/form-schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { defaultFromValues } from "./business";
+import "./App.css";
 
 function App() {
-  const methods = useForm<FormFields>({
-    defaultValues: defaultFromValues,
-    resolver: zodResolver(schema),
-    reValidateMode: "onBlur",
-    mode: "onBlur",
-  });
+	const methods = useForm<FormFields>({
+		defaultValues: defaultFromValues,
+		resolver: zodResolver(schema),
+		reValidateMode: "onBlur",
+		mode: "onBlur",
+	});
 
-  return (
-    <FormProvider {...methods}>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <Container maxWidth="sm">
-          <form>
-            <HorizontalStepper />
-          </form>
-          <DevTool control={methods.control} />
-        </Container>
-      </LocalizationProvider>
-    </FormProvider>
-  );
+	return (
+		<FormProvider {...methods}>
+			<LocalizationProvider dateAdapter={AdapterDayjs}>
+				<Container maxWidth="sm" sx={{ p: 2 }}>
+					<form>
+						<HorizontalStepper />
+					</form>
+					<DevTool control={methods.control} />
+				</Container>
+			</LocalizationProvider>
+		</FormProvider>
+	);
 }
 
 export default App;
