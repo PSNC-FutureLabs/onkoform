6a8838b2ce37be6c715e0ff7ed4bffa46635f833
DINK74 <37271094+DINK74@users.noreply.github.com>
2024-05-23 09:43:39 +0200

    ThemeProvider implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import "dayjs/locale/pl";
@@ -9,6 +10,47 @@ import { FormFields, schema } from "./business/form-schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { defaultFromValues } from "./business";
 import "./App.css";
+import { lightGreen, grey } from "@mui/material/colors";
+
+const theme = createTheme({
+	typography: {
+		fontFamily: "'Bio Sans', sans-serif",
+		h1: {
+			color: "gray",
+			fontSize: "48px",
+			fontWeight: "700",
+		},
+		h3: {
+			color: "gray",
+			fontSize: "20px",
+			fontWeight: "400",
+		},
+	},
+	palette: {
+		background: {
+			default: grey[800],
+		},
+		primary: {
+			main: "#000",
+			light: grey[800],
+			dark: grey[800],
+			contrastText: "#F7F9FC",
+		},
+		secondary: {
+			main: lightGreen[500],
+			contrastText: grey[800],
+		},
+		success: {
+			main: lightGreen[500],
+			contrastText: grey["A100"],
+		},
+		info: {
+			main: lightGreen[500],
+			light: lightGreen[900],
+			dark: lightGreen[200],
+		},
+	},
+});
 
 function App() {
 	const methods = useForm<FormFields>({
@@ -19,16 +61,18 @@ function App() {
 	});
 
 	return (
-		<FormProvider {...methods}>
-			<LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pl">
-				<Container maxWidth="sm" sx={{ p: 2 }}>
-					<form>
-						<HorizontalStepper />
-					</form>
-					<DevTool control={methods.control} />
-				</Container>
-			</LocalizationProvider>
-		</FormProvider>
+		<ThemeProvider theme={theme}>
+			<FormProvider {...methods}>
+				<LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pl">
+					<Container maxWidth="sm" sx={{ p: 2 }}>
+						<form>
+							<HorizontalStepper />
+						</form>
+						<DevTool control={methods.control} />
+					</Container>
+				</LocalizationProvider>
+			</FormProvider>
+		</ThemeProvider>
 	);
 }
 
