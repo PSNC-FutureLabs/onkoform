fcb54e865e9be8a1d9b5150e5647657c4ea03a26
DINK74 <37271094+DINK74@users.noreply.github.com>
2024-05-23 04:48:30 +0200

    temperature field customization (draft)

--- src/components/form-components/FormInputNumber.tsx
+++ src/components/form-components/FormInputNumber.tsx
@@ -1,36 +1,40 @@
 import { Controller } from "react-hook-form";
 import { FormInputProps } from "../../business/types";
 import { TextField } from "@mui/material";
+import { InputAdornment } from "@mui/material";
 
-export const FormInputNumber = ({
-  name,
-  control,
-  label,
-  placeholder,
-}: FormInputProps) => {
-  return (
-    <Controller
-      name={name}
-      control={control}
-      render={({
-        field: { onChange, onBlur, ref, value },
-        fieldState: { error },
-      }) => (
-        <TextField
-          value={value}
-          onChange={(e) =>
-            onChange(e.target.value === "" ? null : Number(e.target.value))
-          }
-          onBlur={onBlur}
-          ref={ref}
-          label={label}
-          type="number"
-          error={!!error}
-          helperText={error?.message}
-          placeholder={placeholder}
-          style={{ width: "100%" }}
-        />
-      )}
-    />
-  );
+export const FormInputNumber = ({ name, control, label, placeholder }: FormInputProps) => {
+	return (
+		<Controller
+			name={name}
+			control={control}
+			render={({ field: { onChange, onBlur, ref, value }, fieldState: { error } }) => (
+				<TextField
+					InputProps={
+						name === "temperature"
+							? {
+									endAdornment: <InputAdornment position="end">&deg;C</InputAdornment>,
+							  }
+							: {}
+					}
+					inputProps={
+						name === "temperature"
+							? {
+									step: 0.1,
+							  }
+							: {}
+					}
+					value={value}
+					onChange={(e) => onChange(e.target.value === "" ? null : Number(e.target.value))}
+					onBlur={onBlur}
+					ref={ref}
+					label={label}
+					type="number"
+					error={!!error}
+					helperText={error?.message}
+					placeholder={placeholder}
+				/>
+			)}
+		/>
+	);
 };
