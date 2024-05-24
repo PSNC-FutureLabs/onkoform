8de6c041353f07e09268382167ef37ce6e6a9d88
DINK74 <37271094+DINK74@users.noreply.github.com>
2024-05-24 10:59:18 +0200

    draft v2

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,7 +1,14 @@
-import { Box, Stack, Button, Typography } from "@mui/material";
+import { Box, Stack, Button, Typography, Icon } from "@mui/material";
 import imageMainBackground from "../../public/images/desktop-main-background.png";
 import imageBoyGirlTeddyBear from "../../public/images/boy-girl-teddybear.svg";
 import imageMomKid from "../../public/images/mom-kid.svg";
+import imageLogo from "../../public/images/logo-mm.svg";
+import MonitorHeartIcon from "@mui/icons-material/MonitorHeart";
+import DeviceThermostatIcon from "@mui/icons-material/DeviceThermostat";
+import SickIcon from "@mui/icons-material/Sick";
+import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
+import QueryStatsIcon from "@mui/icons-material/QueryStats";
+
 import { grey } from "@mui/material/colors";
 
 interface LandingPageProps {
@@ -21,12 +28,12 @@ export default function LandingPage({ onClickStart }: LandingPageProps) {
 						backgroundRepeat: "no-repeat",
 					}}
 				>
-					<Stack direction="row">
+					<Stack direction="row" alignItems="center">
 						<Box
 							component="img"
 							sx={{
-								width: 100,
-								height: 100,
+								width: 300,
+								height: 300,
 							}}
 							src={imageBoyGirlTeddyBear}
 							alt="chłopiec, dziewczynka, pluszowy miś"
@@ -48,8 +55,8 @@ export default function LandingPage({ onClickStart }: LandingPageProps) {
 						<Box
 							component="img"
 							sx={{
-								width: 100,
-								height: 100,
+								width: 300,
+								height: 300,
 							}}
 							src={imageMomKid}
 							alt="mama i dziecko"
@@ -58,14 +65,14 @@ export default function LandingPage({ onClickStart }: LandingPageProps) {
 				</Box>
 			</Box>
 			<Box component="section">
-				<Stack direction="row">
-					<Box p={4}>
-						<Typography variant="body1">
+				<Stack direction="row" px={6} py={4}>
+					<Box p={4} sx={{ flex: 1 }}>
+						<Typography variant="h4" color="black">
 							W prosty i intuicyjny sposób zweryfikuj niepokojące objawy i wyniki badań laboratoryjnych
 							Twojego dziecka.
 						</Typography>
 					</Box>
-					<Box p={4}>
+					<Box p={4} sx={{ flex: 1 }}>
 						<Typography variant="body2">
 							Dowiedz się czy konieczne jest powtórzenie badań lub pilna konsultacja z lekarzem. Jeśli tak
 							- otrzymasz także potrzebny numer telefonu. Zajmie Ci to jedynie około trzech minut.
@@ -75,43 +82,74 @@ export default function LandingPage({ onClickStart }: LandingPageProps) {
 			</Box>
 			<Box component="section">
 				<Stack sx={{ backgroundColor: grey[100] }} p={1}>
-					<Typography variant="h3" align="center" p={2} color="black">
+					<Typography variant="h3" align="center" pt={4} color="black" fontWeight={700}>
 						Przygotuj
 					</Typography>
 					<Stack direction="row" p={6}>
 						<Box sx={{ flex: 1 }}>
-							<Typography variant="h6" align="center" p={2} color="black">
-								Dane pacjenta
-							</Typography>
-							<Typography variant="body2" align="center">
-								Podstawowe dane, takie jak data urodzenia czy wiodący oddział szpitalny.
-							</Typography>
+							<Stack alignItems="center">
+								<LocalHospitalIcon />
+								<Typography variant="h6" align="center" p={2} color="black">
+									Dane pacjenta
+								</Typography>
+								<Typography variant="body2" align="center">
+									Podstawowe dane, takie jak data urodzenia czy wiodący oddział szpitalny.
+								</Typography>
+							</Stack>
 						</Box>
 						<Box sx={{ flex: 1 }}>
-							<Typography variant="h6" align="center" p={2} color="black">
-								Zaobserwowane objawy
-							</Typography>
-							<Typography variant="body2" align="center">
-								Temperatura i niepokojące symptomy np. dreszcze, toksyczności śluzówkowe.{" "}
-							</Typography>
+							<Stack alignItems="center">
+								<SickIcon/>
+								<Typography variant="h6" align="center" p={2} color="black">
+									Zaobserwowane objawy
+								</Typography>
+								<Typography variant="body2" align="center">
+									Temperatura i niepokojące symptomy np. dreszcze, toksyczności śluzówkowe.{" "}
+								</Typography>
+							</Stack>
 						</Box>
 						<Box sx={{ flex: 1 }}>
-							<Typography variant="h6" align="center" p={2} color="black">
-								Aktualne i poprzednie badania{" "}
-							</Typography>
-							<Typography variant="body2" align="center">
-								Wyniki badań laboratoryjnych (HGB, WBC, PLT, ALT, AST, NEUT).{" "}
-							</Typography>
+							<Stack alignItems="center">
+								<QueryStatsIcon/>
+								<Typography variant="h6" align="center" p={2} color="black">
+									Aktualne i poprzednie badania{" "}
+								</Typography>
+								<Typography variant="body2" align="center">
+									Wyniki badań laboratoryjnych (HGB, WBC, PLT, ALT, AST, NEUT).{" "}
+								</Typography>
+							</Stack>
 						</Box>
 					</Stack>
-					<Box display="flex" justifyContent="center" p={2}>
+					<Box display="flex" justifyContent="center" pb={6}>
 						<Button variant="contained" onClick={onClickStart}>
 							Przejdź do formularza
 						</Button>
 					</Box>
 				</Stack>
 			</Box>
-			<Box component="section"></Box>
+			<Box component="section">
+				<Stack direction="row" px={6} py={4} justifyContent="space-around">
+					<Box p={4} sx={{ width: "40%" }}>
+						<Typography variant="h4" color="black">
+							Tworzymy przestrzeń wsparcia dla{" "}
+							<strong>dzieci z chorobami nowotworowymi i ich opiekunów.</strong>
+						</Typography>
+						<Typography variant="body2">
+							Dzięki zaangażowaniu doświadczonych lekarzy, programistów i grafików pomagamy lepiej
+							zrozumieć sytuację zdrowotną dziecka oraz podjąć odpowiednie kroki w odpowiednim czasie.
+						</Typography>
+					</Box>
+					<Box
+						component="img"
+						sx={{
+							width: 300,
+							height: 300,
+						}}
+						src={imageMomKid}
+						alt="mama i dziecko"
+					/>
+				</Stack>
+			</Box>
 			<Box component="section">
 				<Box
 					sx={{
@@ -122,17 +160,46 @@ export default function LandingPage({ onClickStart }: LandingPageProps) {
 						alignItems: "center",
 						justifyContent: "center",
 					}}
+					px={22}
 				>
-					<Stack>
-						<Typography variant="h6" align="center">
-							MaliMocni
-						</Typography>
-						<Typography variant="h4" align="center">
+					<Stack alignItems="center">
+						<Box
+							component="img"
+							sx={{
+								width: 300,
+								height: 100,
+							}}
+							src={imageLogo}
+							alt="logo MaliMocni"
+						/>
+						<Typography variant="h2" align="center">
 							Rodzic zna swoje dziecko najlepiej i często jako pierwszy dostrzega niepokojące objawy.
 							Dlatego zapewniamy mu wsparcie, aby mógł szybko i pewnie reagować.
 						</Typography>
 					</Stack>
 				</Box>
+			</Box>
+			<Box component="section">
+				<Stack direction="row" px={6} py={4} justifyContent="space-around">
+					<Box
+						component="img"
+						sx={{
+							width: 300,
+							height: 300,
+						}}
+						src={imageBoyGirlTeddyBear}
+						alt="mama i dziecko"
+					/>
+					<Box p={4} sx={{ width: "40%" }}>
+						<Typography variant="h4" color="black">
+							Kim są Mali Mocni?
+						</Typography>
+						<Typography variant="body2">
+							Mali Mocni to prawdziwi bohaterowie, wykazujący się niezwykłą odwagą i siłą w obliczu walki
+							z chorobami onkologicznymi.
+						</Typography>
+					</Box>
+				</Stack>
 			</Box>
 			<Box component="footer" sx={{ backgroundColor: grey[900] }}>
 				<Stack direction="row" alignContent="space-between" justifyContent="space-between">
