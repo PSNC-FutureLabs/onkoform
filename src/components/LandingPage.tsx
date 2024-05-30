5a44bad0c0550efd24c80a156797e03e5ae266e3
Krzysztof Wlodarski <krzysztof.wlodarski@capgemini.com>
2024-05-30 23:18:38 +0200

    section 'Preparation' draft implemented

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -18,12 +18,12 @@ interface LandingPageProps {
 	onClickStart: () => void;
 }
 
-export default function LandingPage() {
+export default function LandingPage({ onClickStart }: LandingPageProps) {
 	return (
 		<>
 			<Grid
 				container
-				component="main"
+				component="header"
 				sx={{
 					backgroundImage: {
 						xs: `url(${imageMobileMainBackground})`,
@@ -36,12 +36,22 @@ export default function LandingPage() {
 				pt={{ xs: 2, sm: 10 }}
 				pb={10}
 			>
+				<Grid item xs={12}>
+					<Box
+						component="img"
+						display={{ xs: "block", sm: "none" }}
+						sx={{
+							width: 200,
+						}}
+						src={imageLogo}
+						alt="Logo projektu Mali Mocni"
+					/>
+				</Grid>
 				<Grid item xs={12} sm={3}>
 					<Box
 						component="img"
 						sx={{
-							width: 300,
-							height: 300,
+							width: 200,
 						}}
 						src={imageBoyGirlTeddyBear}
 						alt="chłopiec, dziewczynka, pluszowy miś"
@@ -50,8 +60,7 @@ export default function LandingPage() {
 						component="img"
 						display={{ xs: "block", sm: "none" }}
 						sx={{
-							width: 300,
-							height: 300,
+							width: 200,
 						}}
 						src={imageMomKid}
 						alt="mama i dziecko"
@@ -67,7 +76,9 @@ export default function LandingPage() {
 							dziecka
 						</Typography>
 						<Box display="flex" justifyContent="center" mb={2}>
-							<Button variant="contained">Rozpocznij</Button>
+							<Button variant="contained" onClick={onClickStart}>
+								Rozpocznij
+							</Button>
 						</Box>
 					</Stack>
 				</Grid>
@@ -76,8 +87,7 @@ export default function LandingPage() {
 						component="img"
 						display={{ xs: "none", sm: "block" }}
 						sx={{
-							width: 300,
-							height: 300,
+							width: 200,
 						}}
 						src={imageMomKid}
 						alt="mama i dziecko"
@@ -92,12 +102,63 @@ export default function LandingPage() {
 					</Typography>
 				</Box>
 			</Grid>
-			<Grid item component="section" xs={12} sm={6} sx={{ backgroundColor: grey[200] }}>
+			<Grid item component="section" xs={12} sm={6} sx={{ backgroundColor: "white" }}>
 				<Box p={{ xs: 2, sm: 6 }}>
 					<Typography variant="body1">
 						Dowiedz się czy konieczne jest powtórzenie badań lub pilna konsultacja z lekarzem. Jeśli tak -
 						otrzymasz także potrzebny numer telefonu. Zajmie Ci to jedynie około trzech minut.
 					</Typography>
+				</Box>
+			</Grid>
+			<Grid item component="section" xs={12} sx={{ backgroundColor: grey[100] }}>
+				<Typography variant="h2" align="center" pt={4} color="black" fontWeight={700}>
+					Przygotuj
+				</Typography>
+			</Grid>
+			<Grid item component="section" xs={12} sm={4} sx={{ backgroundColor: grey[100] }}>
+				<Box sx={{ flex: 1 }}>
+					<Stack alignItems="center">
+						<LocalHospitalIcon fontSize="large" />
+						<Typography variant="h6" align="center" p={2} color="black">
+							Dane pacjenta
+						</Typography>
+						<Typography variant="body2" align="center">
+							Podstawowe dane, takie jak data urodzenia czy wiodący oddział szpitalny.
+						</Typography>
+					</Stack>
+				</Box>
+			</Grid>
+			<Grid item component="section" xs={12} sm={4} sx={{ backgroundColor: grey[100] }}>
+				<Box sx={{ flex: 1 }}>
+					<Stack alignItems="center">
+						<SickIcon fontSize="large" />
+						<Typography variant="h6" align="center" p={2} color="black">
+							Zaobserwowane objawy
+						</Typography>
+						<Typography variant="body2" align="center">
+							Temperatura i niepokojące symptomy np. dreszcze, toksyczności śluzówkowe.{" "}
+						</Typography>
+					</Stack>
+				</Box>
+			</Grid>
+			<Grid item component="section" xs={12} sm={4} sx={{ backgroundColor: grey[100] }}>
+				<Box sx={{ flex: 1 }}>
+					<Stack alignItems="center">
+						<QueryStatsIcon fontSize="large" />
+						<Typography variant="h6" align="center" p={2} color="black">
+							Aktualne i poprzednie badania{" "}
+						</Typography>
+						<Typography variant="body2" align="center">
+							Wyniki badań laboratoryjnych (HGB, WBC, PLT, ALT, AST, NEUT).{" "}
+						</Typography>
+					</Stack>
+				</Box>
+			</Grid>
+			<Grid item component="section" xs={12} sx={{ backgroundColor: grey[100] }}>
+				<Box display="flex" justifyContent="center" pb={6}>
+					<Button variant="contained" onClick={onClickStart}>
+						Przejdź do formularza
+					</Button>
 				</Box>
 			</Grid>
 			<Grid item component="footer" xs={12} sx={{ backgroundColor: grey[900] }}>
