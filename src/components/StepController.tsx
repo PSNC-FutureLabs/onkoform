041051c98606b639d1ee4bb0073320f3a1d81ec0
Krzysztof Wlodarski <krzysztof.wlodarski@capgemini.com>
2024-06-11 01:02:05 +0200

    steps visualization implemented (draft)

--- src/components/StepController.tsx
+++ src/components/StepController.tsx
@@ -1,20 +1,28 @@
 import { useState } from "react";
-import { Grid, Box, Stack, Link, Typography } from "@mui/material";
+import { Grid, Box, Stack, Typography } from "@mui/material";
 import { FieldValues, SubmitErrorHandler, SubmitHandler, useFormContext } from "react-hook-form";
 import imageDesktopFormSideBackground from "/images/desktop-form-side-background.png";
 import imageMobileFormTopBackground from "/images/mobile-form-top-background.png";
 import imageLogo from "/images/logo-mm.svg";
 import LandingPage from "./LandingPage";
-import { grey } from "@mui/material/colors";
+import Footer from "./Footer";
+import StraightIcon from "@mui/icons-material/Straight";
+import CheckIcon from '@mui/icons-material/Check';
+
+type Step = {};
+
+const stepLandingPage = 0;
+
+const steps = ["Informacje o pacjencie", "Niepokojące objawy", "Aktualne badania", "Poprzednie badania"];
 
 export default function StepController() {
-	const [isLandingPageActive, setIsLandingPageActive] = useState<boolean>(true);
+	const [activeStep, setActiveStep] = useState<number>(stepLandingPage);
 
 	const handleStart = () => {
-		setIsLandingPageActive(false);
+		setActiveStep(stepLandingPage + 1);
 	};
 
-	if (isLandingPageActive) {
+	if (activeStep === stepLandingPage) {
 		return <LandingPage onClickStart={handleStart} />;
 	}
 
@@ -31,61 +39,79 @@ export default function StepController() {
 							sm: `url(${imageDesktopFormSideBackground})`,
 						},
 						backgroundSize: "cover",
-						backgroundPosition: "center",
+						backgroundPosition: "top",
 						backgroundRepeat: "no-repeat",
 					}}
 				>
-					<Box
-						component="img"
-						src={imageLogo}
-						alt="logo MaliMocni"
-						sx={{
-							height: { xs: 20, sm: 50 },
-						}}
-					/>
-				</Grid>
-				<Grid item xs={12} sm={8}></Grid>
-			</Grid>
-			<Grid item component="footer" xs={12} sx={{ backgroundColor: grey[900] }}>
-				<Stack
-					direction="row"
-					alignContent="space-between"
-					justifyContent="space-between"
-					sx={{ width: "100%" }}
-				>
-					<Box p={1}>
-						<Typography variant="body2" align="center" color="white">
-							&copy; 2024 by{" "}
-							<Link
-								href="https://psnc.pl"
-								target="_blank"
-								rel="noreferrer"
-								color="inherit"
-								underline="hover"
-							>
-								PSNC
-							</Link>
-							. All rights reserved.
-						</Typography>
-					</Box>
-					<Stack direction="row">
-						<Box>
-							<Typography variant="body2" align="center" color="white" p={1}>
-								<Link href="#" color="inherit" underline="hover">
-									Privacy policy
-								</Link>
-							</Typography>
-						</Box>
-						<Box>
-							<Typography variant="body2" align="center" color="white" p={1}>
-								<Link href="#" color="inherit" underline="hover">
-									Terms & Conditions
-								</Link>
-							</Typography>
-						</Box>
+					<Stack mb={4}>
+						<Box
+							component="img"
+							src={imageLogo}
+							alt="logo MaliMocni"
+							sx={{
+								height: { xs: 20, sm: 40 },
+							}}
+							mt={2}
+							mb={4}
+						/>
+						{steps.map((step, idx) => (
+							<Stack color="white">
+								<Stack direction="row">
+									<Box
+										width={56}
+										height={56}
+										border={2}
+										borderRadius="50%"
+										display="flex"
+										alignItems="center"
+										justifyContent="center"
+										position="relative"
+									>
+										<Box
+											width={43}
+											height={43}
+											borderRadius="50%"
+											display="flex"
+											alignItems="center"
+											justifyContent="center"
+											position="absolute"
+											sx={{
+												backgroundColor: idx + 1 === activeStep ? "white" : "transparent",
+											}}
+										>
+											<Typography
+												variant="h6"
+												color={idx + 1 === activeStep ? "green" : "inherit"}
+											>
+												{idx + 1}
+											</Typography>
+										</Box>
+									</Box>
+									<Box pl={2}>
+										<Typography variant="body2">
+											Krok {idx + 1}/{steps.length}
+										</Typography>
+										<Typography variant="h5">{step}</Typography>
+									</Box>
+								</Stack>
+								{idx < steps.length - 1 ? (
+									<Box py={2}>
+										<StraightIcon
+											fontSize="large"
+											color="inherit"
+											sx={{ transform: "rotate(180deg)" }}
+										/>
+									</Box>
+								) : (
+									""
+								)}
+							</Stack>
+						))}
 					</Stack>
-				</Stack>
+				</Grid>
+				<Grid item xs={12} sm={8} sx={{ backgroundColor: "white" }}></Grid>
 			</Grid>
+			<Footer />
 		</>
 	);
 }
