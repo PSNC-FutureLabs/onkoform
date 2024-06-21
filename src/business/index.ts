3aaba5250c609fae392d9f51fe624c8090b83553
Krzysztof Wlodarski <krzysztof.wlodarski@capgemini.com>
2024-06-21 12:28:15 +0200

    version patch number changed

--- src/business/index.ts
+++ src/business/index.ts
@@ -4,7 +4,7 @@ import { DropdownOptionsType, MultiCheckboxOptionsType, RadioOptionsType, UnitTy
 export const versionTag = {
 	majorNo: 0,
 	minorNo: 3,
-	patchNo: 0,
+	patchNo: 1,
 	status: "alpha",	
 	date: "2024.06.21",
 };
