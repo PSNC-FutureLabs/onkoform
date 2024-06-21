f480899436d0ec8cf82468a6d5ad0c5002635e94
Krzysztof Wlodarski <krzysztof.wlodarski@capgemini.com>
2024-06-21 09:20:36 +0200

    getAgeValue replaced with getCurrentAge

--- src/components/Summary/helpers.ts
+++ src/components/Summary/helpers.ts
@@ -1,23 +1,31 @@
-import {
-  diseaseOptions,
-  genderOptions,
-  hospitalWardOptions,
-} from "../../business";
+import { diseaseOptions, genderOptions, hospitalWardOptions } from "../../business";
 
 export const getGenderValue = (gender: string) => {
-  return genderOptions.find((opt) => opt.value == gender)?.label;
+	return genderOptions.find((opt) => opt.value == gender)?.label;
 };
 
-export const getAgeValue = (birthday: string) => {
-  const currentYear = new Date().getFullYear();
-  const birthYear = new Date(birthday).getFullYear();
-  return currentYear - birthYear;
+export const getCurrentAge = (dateOfBirth: string) => {
+	const dob = new Date(dateOfBirth);
+	const now = new Date();
+
+	let years = now.getFullYear() - dob.getFullYear();
+	let months = now.getMonth() - dob.getMonth();
+
+	if (months < 0) {
+		years--;
+		months += 12;
+	}
+
+	return {
+		years: years,
+		months: months,
+	};
 };
 
 export const getDiseaseValue = (disease: string) => {
-  return diseaseOptions.find((opt) => opt.value == disease)?.label;
+	return diseaseOptions.find((opt) => opt.value == disease)?.label;
 };
 
 export const getHospitalWardValue = (hospitalWard: string) => {
-  return hospitalWardOptions.find((opt) => opt.value == hospitalWard)?.label;
+	return hospitalWardOptions.find((opt) => opt.value == hospitalWard)?.label;
 };
