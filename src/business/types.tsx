144a015fd1228542174221efd6a5afc1b0f7bc70
Krzysztof Wlodarski <krzysztof.wlodarski@capgemini.com>
2024-06-22 01:15:05 +0200

    SymptomValues added

--- src/business/types.tsx
+++ src/business/types.tsx
@@ -1,50 +1,46 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import {
-  Control,
-  FieldValues,
-  UseFormGetValues,
-  UseFormSetValue,
-} from "react-hook-form";
+import { Control, FieldValues, UseFormGetValues, UseFormSetValue } from "react-hook-form";
 
 export type RadioOptionsType = {
-  label: string;
-  value: string;
+	label: string;
+	value: string;
 };
 
 export type MultiCheckboxOptionsType = {
-  label: string;
-  value: string;
+	label: string;
+	value: string;
 };
 
 export type DropdownOptionsType = {
-  label: string;
-  value: string;
+	label: string;
+	value: string;
 };
 
 export type UnitType = "g/dl" | "mmol/l" | "%" | "μl";
 
 export type FormInputProps = {
-  name: string;
-  control: Control<FieldValues, any>;
-  label: string;
-  placeholder?: string;
+	name: string;
+	control: Control<FieldValues, any>;
+	label: string;
+	placeholder?: string;
 };
 
 export type InputRadioProps = FormInputProps & {
-  options: RadioOptionsType[];
+	options: RadioOptionsType[];
 };
 export type InputDropdownProps = FormInputProps & {
-  options: DropdownOptionsType[];
+	options: DropdownOptionsType[];
 };
 
 export type FormInputMultiCheckboxProps = FormInputProps & {
-  setValue: UseFormSetValue<FieldValues>;
-  getValues: UseFormGetValues<FieldValues>;
+	setValue: UseFormSetValue<FieldValues>;
+	getValues: UseFormGetValues<FieldValues>;
 };
 
 export type MarkerRowProps = {
-  control: Control<FieldValues, any>;
-  markerName: string;
-  label: string;
-  options: UnitType[];
+	control: Control<FieldValues, any>;
+	markerName: string;
+	label: string;
+	options: UnitType[];
 };
+
