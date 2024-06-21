833c80278180ae19d0ba9228a3902cb55eb8529a
Krzysztof Wlodarski <krzysztof.wlodarski@capgemini.com>
2024-06-21 13:43:22 +0200

    Selected symptoms turned into text

--- src/components/Summary/AlarmingSymptoms.tsx
+++ src/components/Summary/AlarmingSymptoms.tsx
@@ -1,9 +1,16 @@
 import { Grid, Stack, Typography } from "@mui/material";
 import { BasicInfoCard } from "./BasicInfoCard";
 import { useFormContext } from "react-hook-form";
+import { symptomsOptions } from "../../business";
 
 export const AlarmingSymptoms = () => {
 	const { getValues } = useFormContext();
+
+	const symptoms = getValues("symptoms");
+	const symptomsText = symptomsOptions
+		.filter((option) => symptoms.includes(option.value))
+		.map((option) => option.label)
+		.join(", ");
 
 	return (
 		<Stack mt={3} pt={2} borderTop={1}>
@@ -12,7 +19,7 @@ export const AlarmingSymptoms = () => {
 			</Typography>
 			<Grid container spacing={2}>
 				<BasicInfoCard label="Temperatura ciała" value={getValues("temperature") + " °C"} />
-				<BasicInfoCard label="Symptomy towarzyszące" value="Brak" />
+				<BasicInfoCard label="Symptomy towarzyszące" value={symptomsText} />
 			</Grid>
 		</Stack>
 	);
