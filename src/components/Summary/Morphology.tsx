aad9d5bb42327d4d46983602aa8536b35a0c9c30
Krzysztof Wlodarski <krzysztof.wlodarski@capgemini.com>
2024-06-21 07:55:27 +0200

    typo fixed

--- src/components/Summary/Morphology.tsx
+++ src/components/Summary/Morphology.tsx
@@ -2,23 +2,17 @@ import { Grid, Typography } from "@mui/material";
 import { MorphologyCard } from "./MorphologyCard";
 
 export const Morphology = () => {
-  return (
-    <div className="summary-section">
-      <Typography
-        style={{
-          textAlign: "left",
-          paddingBottom: "24px",
-        }}>
-        Zestawienie wyników z badań morfologi krwi
-      </Typography>
-      <Grid container spacing={2}>
-        <MorphologyCard markerName="HGB" />
-        <MorphologyCard markerName="WBC" />
-        <MorphologyCard markerName="PLT" />
-        <MorphologyCard markerName="ALT" />
-        <MorphologyCard markerName="AST" />
-        <MorphologyCard markerName="NEUT" />
-      </Grid>
-    </div>
-  );
+	return (
+		<div className="summary-section">
+			<Typography variant="h5" color="black">Zestawienie wyników z badań morfologii krwi</Typography>
+			<Grid container spacing={2}>
+				<MorphologyCard markerName="HGB" />
+				<MorphologyCard markerName="WBC" />
+				<MorphologyCard markerName="PLT" />
+				<MorphologyCard markerName="ALT" />
+				<MorphologyCard markerName="AST" />
+				<MorphologyCard markerName="NEUT" />
+			</Grid>
+		</div>
+	);
 };
