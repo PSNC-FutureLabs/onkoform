7d2eaf61b79a652a91cef65b238ae03b14fe0b4e
Krzysztof Wlodarski <krzysztof.wlodarski@capgemini.com>
2024-06-21 12:28:30 +0200

    Diagnoses implemented

--- src/components/Summary/Result.tsx
+++ src/components/Summary/Result.tsx
@@ -2,7 +2,17 @@ import { Stack, Alert, Typography } from "@mui/material";
 import { OverridableStringUnion } from "@mui/types";
 import { AlertColor, AlertPropsColorOverrides } from "@mui/material";
 
+enum DiagnoseLevel {
+	Unconclusive,
+	NoAction,
+	RepeatTestIn3Days,
+	RepeatTest,
+	Consultation,
+	UrgentConsultation,
+}
+
 type Diagnose = {
+	level: DiagnoseLevel;
 	header: string;
 	body: string;
 	severity: OverridableStringUnion<AlertColor, AlertPropsColorOverrides>;
@@ -12,27 +22,54 @@ type Diagnoses = Array<Diagnose>;
 
 const DiagnosesDefinitions: Diagnoses = [
 	{
-		header: "Wyniki wymagają pilnej konsultacji z lekarzem.",
-		body: "Prosimy o natychmiastowy kontakt z placówką medyczną w celu uzyskania profesjonalnej oceny i pomocy. Poinformuj lekarza o wyniku HGB, ALT oraz AST.",
-		severity: "error"
+		level: DiagnoseLevel.Unconclusive,
+		header: "Brak diagnozy",
+		body: "Aplikacja nie jest w stanie określić wyniku. Skontaktuj się z dostawcą.",
+		severity: "warning",
 	},
 	{
+		level: DiagnoseLevel.NoAction,
+		header: "Wyniki są w poprawne",
+		body: "",
+		severity: "success",
+	},
+	{
+		level: DiagnoseLevel.RepeatTestIn3Days,
+		header: "Wyniki wymagają uwagi",
+		body: "Wykonaj ponowne badanie za 3 dni",
+		severity: "info",
+	},
+	{
+		level: DiagnoseLevel.RepeatTest,
+		header: "Wyniki wymagają uwagi",
+		body: "Wykonaj ponowne badanie w najbliższym terminie",
+		severity: "warning",
+	},
+	{
+		level: DiagnoseLevel.Consultation,
+		header: "Wyniki wymagają uwagi",
+		body: "Skonsultuj się z lekarzem z kliniki",
+		severity: "warning",
+	},
+	{
+		level: DiagnoseLevel.UrgentConsultation,
 		header: "Wyniki wymagają pilnej konsultacji z lekarzem.",
-		body: "Prosimy o natychmiastowy kontakt z placówką medyczną w celu uzyskania profesjonalnej oceny i pomocy. Poinformuj lekarza o wyniku HGB, ALT oraz AST.",
-		severity: "error"
-	}
+		body: "Zapoznaj się ze szczegółami poniżej",
+		severity: "error",
+	},
 ];
 
 export const Result = () => {
-	let diagnoseId : number = 0;
+	const calculatedDiagnoseLevel: DiagnoseLevel = 0;
 
-	diagnoseId++;
-	
+	const diagnose =
+		DiagnosesDefinitions.find((item) => item.level === calculatedDiagnoseLevel) ?? DiagnosesDefinitions[DiagnoseLevel.Unconclusive];
+
 	return (
 		<Stack>
-			<Alert severity={DiagnosesDefinitions[diagnoseId].severity}>
-				<Typography style={{ fontWeight: "500" }}>{DiagnosesDefinitions[diagnoseId].header}</Typography>
-				{DiagnosesDefinitions[diagnoseId].body}
+			<Alert severity={diagnose.severity}>
+				<Typography style={{ fontWeight: "500" }}>{diagnose.header}</Typography>
+				{diagnose.body}
 			</Alert>
 		</Stack>
 	);
