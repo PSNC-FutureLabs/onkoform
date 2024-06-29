88a613e49f922516504a249abdb6100c01c2a2ca
Krzysztof Wlodarski <krzysztof.wlodarski@capgemini.com>
2024-06-29 12:35:20 +0200

    minHeight set to 100vh on non-mobile

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -63,7 +63,7 @@ export default function LandingPage({ onClickStart }: LandingPageProps) {
 				pt={{ xs: 2, sm: 0 }}
 				pb={{ xs: 4, sm: 0 }}
 				color="white"
-				height={{ sm: 1080 }}
+				minHeight={{ sm: "100vh" }}
 				alignItems="flex-start"
 			>
 				<Grid item xs={12} height="40px">
