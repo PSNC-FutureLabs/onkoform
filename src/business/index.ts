819c4430561bc7a4d12a725b9ab81a163c366f53
Krzysztof Wlodarski <krzysztof.wlodarski@capgemini.com>
2024-07-03 13:00:56 +0200

    versionTag updated

--- src/business/index.ts
+++ src/business/index.ts
@@ -4,7 +4,7 @@ import { DropdownOptionsType, MultiCheckboxOptionsType, RadioOptionsType, UnitTy
 export const versionTag = {
 	majorNo: 0,
 	minorNo: 7,
-	patchNo: 3,
+	patchNo: 4,
 	status: "alpha",
 	date: "2024.07.03",
 };
