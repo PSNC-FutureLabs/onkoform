d4de0ddef63b024bb3e88296736e9f70965ffad1
Krzysztof Wlodarski <krzysztof.wlodarski@capgemini.com>
2024-07-05 13:05:58 +0200

    MedicalParameter re-designed

--- src/business/types.tsx
+++ src/business/types.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Control, FieldValues, UseFormGetValues, UseFormSetValue } from "react-hook-form";
+import { OverridableStringUnion } from "@mui/types";
+import { AlertColor, AlertPropsColorOverrides } from "@mui/material";
 
 type OptionType = {
 	label: string;
@@ -11,15 +13,36 @@ export type RadioOptionsType = OptionType;
 export type MultiCheckboxOptionsType = OptionType;
 export type DropdownOptionsType = OptionType;
 
-export type Markers = "HGB" | "WBC" | "PLT" | "NEUT" | "ALT" | "AST";
+export type BloodMarkersNames = "HGB" | "WBC" | "PLT" | "NEUT" | "ALT" | "AST";
 
-export const MarkerDescriptions: { [key in Markers]: string } = {
-    HGB: "stężenie hemoglobiny",
-    WBC: "liczba białych krwinek (leukocyty)",
-    PLT: "liczba płytek krwi",
-    NEUT: "granulocyty obojętochłonne (neutrofile)",
-    ALT: "poziom aminotransferazy alaninowej",
-    AST: "poziom aminotransferazy asparaginianowej"
+export const BloodMarkerDescriptions: { [key in BloodMarkersNames]: string } = {
+	HGB: "stężenie hemoglobiny",
+	WBC: "liczba białych krwinek (leukocyty)",
+	PLT: "liczba płytek krwi",
+	NEUT: "granulocyty obojętochłonne (neutrofile)",
+	ALT: "poziom aminotransferazy alaninowej (ALAT)",
+	AST: "poziom aminotransferazy asparaginianowej (ASPAT)",
+};
+
+export type BloodMarkers = {
+	[key in BloodMarkersNames]: MedicalParameter;
+};
+
+export type Gender = "male" | "female" | "other";
+
+export type Patient = {
+	dateOfBirth: Date;
+	gender: Gender;
+};
+
+export type Analysis = {
+	patient: Patient;
+	medicalParameters: Array<MedicalParameter>;
+	symptoms: Array<SymptomValues>;
+	diagnose: Diagnose;
+	headacheRating: NullableNumber;
+	painAnxietyRating: NullableNumber;
+	mucosalToxicitiesRating: NullableNumber;
 };
 
 export type UnitType =
@@ -33,6 +56,7 @@ export type UnitType =
 	| "U/l"
 	| "g/dl"
 	| "mg/%"
+	| "mg/dl"
 	| "mmol/l"
 	| "tys./mm³"
 	| "tys./μl"
@@ -83,6 +107,7 @@ export type SymptomValues =
 	| "pain-anxiety";
 
 export type NullableNumber = number | null;
+export type NullableDate = Date | null;
 
 export function inRange(value: NullableNumber, range: string): boolean {
 	if (!value) return false;
@@ -112,46 +137,64 @@ export function inRange(value: NullableNumber, range: string): boolean {
 	}
 }
 
-export class MedicalParameter {
-	actualValue: number;
-	unit: UnitType;
-	referenceValue?: NullableNumber;
-	referenceUnit?: UnitType;
+export function getUnitConversionRatio(baseUnit: UnitType, targetUnit: UnitType): NullableNumber {
+	if (baseUnit === targetUnit) return 1;
 
-	constructor(actualValue: number, unit: UnitType, referenceValue?: number, referenceUnit?: UnitType) {
-		this.actualValue = actualValue;
+	const conversionRatios: Record<string, NullableNumber> = {
+		"mg/dl:mg/%": 1,
+		"g/dl:mg/%": 100,
+		"g/dl:mg/dl": 1000,
+		"mg/%:mg/dl": 10,
+		"mg/%:g/dl": 0.001,
+		"mg/dl:g/dl": 0.001,
+		"mmol/l:mg/dl": 18.01559, // Example for glucose
+		"mg/dl:mmol/l": 1 / 18.01559,
+	};
+
+	const key = `${baseUnit}:${targetUnit}`;
+
+	return conversionRatios[key] || null;
+}
+
+export class MedicalParameter {
+	value: number;
+	unit: UnitType;
+	date?: NullableDate;
+	reference?: MedicalParameter;
+
+	constructor(actualValue: number, unit: UnitType, date?: NullableDate, reference?: MedicalParameter) {
+		this.value = actualValue;
 		this.unit = unit;
-		this.referenceValue = referenceValue ?? null;
-		if (referenceUnit) this.referenceUnit = referenceUnit;
+		this.date = date ?? null;
+		if (reference) {
+			this.reference = reference;
+		}
 	}
 
 	in(unit: UnitType): NullableNumber {
-		if (this.unit === unit) return this.actualValue;
-		return null;
+		if (this.unit === unit) return this.value;
+		const ratio = getUnitConversionRatio(this.unit, unit);
+		return ratio ? ratio * this.value : null;
 	}
 
 	isGrowing(): boolean {
-		return this.referenceValue ? this.actualValue > this.referenceValue : false;
+		return this.reference ? this.value > this.reference.value : false;
 	}
 
 	isDeclining(): boolean {
-		return this.referenceValue ? this.actualValue < this.referenceValue : false;
+		return this.reference ? this.value < this.reference.value : false;
 	}
 
-	getActualValue(unit?: UnitType): NullableNumber {
-		return unit === this.unit ? this.actualValue : null;
+	getValue(): NullableNumber {
+		return this.value;
 	}
 
 	setActualValue(value: number): void {
-		this.actualValue = value;
+		this.value = value;
 	}
 
 	getReferenceValue(): NullableNumber {
-		return this.referenceValue ?? null;
-	}
-
-	setReferenceValue(value: number): void {
-		this.referenceValue = value;
+		return this.reference?.value ?? null;
 	}
 
 	getUnit(): UnitType {
@@ -162,3 +205,60 @@ export class MedicalParameter {
 		this.unit = unit;
 	}
 }
+
+export enum DiagnoseLevel {
+	Unconclusive,
+	OK,
+	RepeatTestIn3Days,
+	RepeatTestIn2Days,
+	ConsultationNeeded,
+	UrgentConsultationNeeded,
+}
+
+export type Diagnose = {
+	level: DiagnoseLevel;
+	header: string;
+	body: string;
+	severity: OverridableStringUnion<AlertColor, AlertPropsColorOverrides>;
+};
+
+export type Diagnoses = Array<Diagnose>;
+
+export const DiagnosesDefinitions: Diagnoses = [
+	{
+		level: DiagnoseLevel.Unconclusive,
+		header: "Brak diagnozy",
+		body: "Aplikacja nie jest w stanie określić wyniku. Skontaktuj się z dostawcą.",
+		severity: "warning",
+	},
+	{
+		level: DiagnoseLevel.OK,
+		header: "Wyniki są w normie",
+		body: "Zapoznaj się ze szczegółami poniżej",
+		severity: "success",
+	},
+	{
+		level: DiagnoseLevel.RepeatTestIn3Days,
+		header: "Powtórz badanie za 3 dni",
+		body: "Zapoznaj się ze szczegółami poniżej",
+		severity: "warning",
+	},
+	{
+		level: DiagnoseLevel.RepeatTestIn2Days,
+		header: "Powtórz badanie za 2 dni",
+		body: "Zapoznaj się ze szczegółami poniżej",
+		severity: "warning",
+	},
+	{
+		level: DiagnoseLevel.ConsultationNeeded,
+		header: "Wyniki wymagają konsultacji z lekarzem.",
+		body: "Zapoznaj się ze szczegółami poniżej",
+		severity: "error",
+	},
+	{
+		level: DiagnoseLevel.UrgentConsultationNeeded,
+		header: "Wyniki wymagają pilnej konsultacji z lekarzem.",
+		body: "Zapoznaj się ze szczegółami poniżej",
+		severity: "error",
+	},
+];
