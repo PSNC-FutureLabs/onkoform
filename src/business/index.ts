690e4f6e96a1ad1b15c5330df5d835d3fcbf48c8
DINK74 <dink74@dink.pl>
2024-07-23 20:43:36 +0200

    versionTag updated

--- src/business/index.ts
+++ src/business/index.ts
@@ -4,7 +4,7 @@ import { DropdownOptionsType, MultiCheckboxOptionsType, RadioOptionsType, UnitTy
 export const versionTag = {
 	majorNo: 0,
 	minorNo: 7,
-	patchNo: 23,
+	patchNo: 24,
 	status: "alpha",
 	date: "2024.07.23",
 };
