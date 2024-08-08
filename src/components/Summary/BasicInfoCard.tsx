41ea238fedc892e70d45c406e295e0918a17268a
Krzysztof Wlodarski <krzysztof.wlodarski@capgemini.com>
2024-08-08 22:46:16 +0200

    responsiveness fixed

--- src/components/Summary/BasicInfoCard.tsx
+++ src/components/Summary/BasicInfoCard.tsx
@@ -2,21 +2,19 @@ import { Grid, Typography } from "@mui/material";
 
 type BasicInfoCardProps = {
 	label: string;
-	value: string | number;
-	optionalText?: string;
+	value: string;
 };
 
-export const BasicInfoCard = ({ label, value, optionalText }: BasicInfoCardProps) => {
+export const BasicInfoCard = ({ label, value }: BasicInfoCardProps) => {
 	return (
 		<>
-			<Grid item xs={6}>
+			<Grid item xs={12} sm={6}>
 				<Typography variant="body1">{label}</Typography>
 			</Grid>
-			<Grid item xs={6}>
+			<Grid item xs={12} sm={6}>
 				<Typography variant="body1">
 					<strong>{value}</strong>
 				</Typography>
-				{optionalText ? <Typography>{optionalText}</Typography> : null}
 			</Grid>
 		</>
 	);
