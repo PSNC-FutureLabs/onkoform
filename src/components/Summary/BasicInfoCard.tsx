afdb149f3fe8e9bead1f56f44b491290009dde2a
Krzysztof Wlodarski <krzysztof.wlodarski@capgemini.com>
2024-08-08 22:38:22 +0200

    layout redesigned

--- src/components/Summary/BasicInfoCard.tsx
+++ src/components/Summary/BasicInfoCard.tsx
@@ -1,4 +1,4 @@
-import { Card, Grid, Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 
 type BasicInfoCardProps = {
 	label: string;
@@ -8,18 +8,16 @@ type BasicInfoCardProps = {
 
 export const BasicInfoCard = ({ label, value, optionalText }: BasicInfoCardProps) => {
 	return (
-		<Grid item xs={6}>
-			<Card elevation={0}
-				style={{
-					width: "90%",
-					textAlign: "left",
-					padding: "12px 10px 12px 16px",
-				}}
-			>
-				<Typography style={{ color: "#555555de" }}>{label}</Typography>
-				<Typography style={{ fontSize: "20px" }}>{value}</Typography>
-				{optionalText ? <Typography style={{ color: "#555555de" }}>{optionalText}</Typography> : null}
-			</Card>
-		</Grid>
+		<>
+			<Grid item xs={6}>
+				<Typography variant="body1">{label}</Typography>
+			</Grid>
+			<Grid item xs={6}>
+				<Typography variant="body1">
+					<strong>{value}</strong>
+				</Typography>
+				{optionalText ? <Typography>{optionalText}</Typography> : null}
+			</Grid>
+		</>
 	);
 };
