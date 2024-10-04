3a418356a62010380a93115219fa40181511437d
DINK74 <dink74@dink.pl>
2024-10-05 01:48:13 +0200

    update versionTag

--- src/business/index.ts
+++ src/business/index.ts
@@ -5,9 +5,9 @@ import { DropdownOptionsType, MultiCheckboxOptionsType, RadioOptionsType, UnitTy
 export const versionTag = {
 	majorNo: 0,
 	minorNo: 8,
-	patchNo: 4,
+	patchNo: 5,
 	status: "alpha",
-	date: "2024.10.02",
+	date: "2024.10.05",
 };
 
 export const versionText = (): string =>
