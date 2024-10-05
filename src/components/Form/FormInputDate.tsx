5163ee74d984b284e051c0d31544fb9979373497
DINK74 <dink74@dink.pl>
2024-10-05 11:40:19 +0200

    disable text input

--- src/components/Form/FormInputDate.tsx
+++ src/components/Form/FormInputDate.tsx
@@ -19,6 +19,7 @@ export const FormInputDate = ({ name, control, label }: FormInputProps) => {
 							error: !!error,
 							helperText: error?.message,
 							onBlur,
+							disabled: true
 						},
 					}}
 				/>
