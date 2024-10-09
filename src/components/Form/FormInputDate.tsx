407ffedb5a23cae89fc59061b64a4a11084f25b9
DINK74 <dink74@dink.pl>
2024-10-09 20:56:26 +0200

    re-enable text input

--- src/components/Form/FormInputDate.tsx
+++ src/components/Form/FormInputDate.tsx
@@ -12,60 +12,47 @@ import "dayjs/locale/uk";
 type SupportedLocales = "en" | "pl" | "ua";
 
 const locales: Record<SupportedLocales, string> = {
-  en: "en",
-  pl: "pl",
-  ua: "uk",
+	en: "en",
+	pl: "pl",
+	ua: "uk",
 };
 
 const localesText: Record<SupportedLocales, any> = {
-  en: enUS.components.MuiLocalizationProvider.defaultProps.localeText,
-  pl: plPL.components.MuiLocalizationProvider.defaultProps.localeText,
-  ua: ukUA.components.MuiLocalizationProvider.defaultProps.localeText,
+	en: enUS.components.MuiLocalizationProvider.defaultProps.localeText,
+	pl: plPL.components.MuiLocalizationProvider.defaultProps.localeText,
+	ua: ukUA.components.MuiLocalizationProvider.defaultProps.localeText,
 };
 
 export const FormInputDate = ({ name, control, label }: FormInputProps) => {
-  const { t, i18n } = useTranslation();
-  /*console.log("Language:", i18n.language);
-  console.log(
-    "LocalesText:",
-    localesText[
-      (i18n.language as keyof typeof localesText) || localesText["pl"]
-    ]
-  );*/
-  return (
-    <Controller
-      name={name}
-      control={control}
-      render={({
-        field: { onChange, value, onBlur, ref },
-        fieldState: { error },
-      }) => (
-        <LocalizationProvider
-          dateAdapter={AdapterDayjs}
-          adapterLocale={
-            locales[(i18n.language as keyof typeof locales) || "pl"]
-          }
-          localeText={
-            localesText[(i18n.language as keyof typeof localesText) || "pl"]
-          }
-        >
-          <DatePicker
-            disableFuture
-            value={value}
-            onChange={onChange}
-            ref={ref}
-            label={label ?? ""}
-            slotProps={{
-              textField: {
-                error: !!error,
-                helperText: error ? <Trans t={t}>{error?.message}</Trans> : " ",
-                onBlur,
-                disabled: true,
-              },
-            }}
-          />
-        </LocalizationProvider>
-      )}
-    />
-  );
+	const { t, i18n } = useTranslation();
+	// console.log("Language:", i18n.language);
+	// console.log("LocalesText:", localesText[(i18n.language as keyof typeof localesText) || localesText["pl"]]);
+	return (
+		<Controller
+			name={name}
+			control={control}
+			render={({ field: { onChange, value, onBlur, ref }, fieldState: { error } }) => (
+				<LocalizationProvider
+					dateAdapter={AdapterDayjs}
+					adapterLocale={locales[(i18n.language as keyof typeof locales) || "pl"]}
+					localeText={localesText[(i18n.language as keyof typeof localesText) || "pl"]}
+				>
+					<DatePicker
+						disableFuture
+						value={value}
+						onChange={onChange}
+						ref={ref}
+						label={label ?? ""}
+						slotProps={{
+							textField: {
+								error: !!error,
+								helperText: error ? <Trans t={t}>{error?.message}</Trans> : " ",
+								onBlur,
+							},
+						}}
+					/>
+				</LocalizationProvider>
+			)}
+		/>
+	);
 };
