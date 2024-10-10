3de2522b72baf30c24a4ba70a6f5d8dfdf334984
Krzysztof Wlodarski <krzysztof.wlodarski@capgemini.com>
2024-10-10 11:19:35 +0200

    refactor with an array of language codes

--- src/components/Form/FormLanguageSwitcher.tsx
+++ src/components/Form/FormLanguageSwitcher.tsx
@@ -4,39 +4,31 @@ import LanguageButton from "./LanguageButton";
 import i18n from "../../utils/i18n";
 
 const LanguageSwitcher: React.FC = () => {
-  const [activeLanguage, setActiveLanguage] = useState<string>(
-    i18n.language ? i18n.language : "pl"
-  );
-  const changeLanguage = (lang: string) => {
-    i18n.changeLanguage(lang);
-    setActiveLanguage(lang);
-  };
+	const languageCodes: Array<string> = ["pl", "ua", "en"];
+	const [activeLanguage, setActiveLanguage] = useState<string>(i18n.language ? i18n.language : "pl");
+	const changeLanguage = (lang: string) => {
+		i18n.changeLanguage(lang);
+		setActiveLanguage(lang);
+	};
 
-  return (
-    <Grid
-      container
-      spacing={1}
-      sx={{
-        maxWidth: "max-content",
-      }}
-    >
-      <LanguageButton
-        lang="pl"
-        activeLanguage={activeLanguage}
-        changeLanguage={changeLanguage}
-      />
-      <LanguageButton
-        lang="ua"
-        activeLanguage={activeLanguage}
-        changeLanguage={changeLanguage}
-      />
-      <LanguageButton
-        lang="en"
-        activeLanguage={activeLanguage}
-        changeLanguage={changeLanguage}
-      />
-    </Grid>
-  );
+	return (
+		<Grid
+			container
+			spacing={1}
+			sx={{
+				maxWidth: "max-content",
+			}}
+		>
+			{languageCodes.map((code) => (
+				<LanguageButton
+					key={code}
+					lang={code}
+					activeLanguage={activeLanguage}
+					changeLanguage={changeLanguage}
+				/>
+			))}
+		</Grid>
+	);
 };
 
 export default LanguageSwitcher;
